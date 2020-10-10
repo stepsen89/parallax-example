a8094ad0cbcc3bec7e2ae7ea0ed8457ed9f0f57a
stepsen89 <stephanie.senoner@gmail.com>
2020-10-10 21:08:20 +0100

    first approach finished

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("loaded")
+
+document.addEventListener("mousemove", (e) => {
+  console.log(e);
+  const laptop = document.querySelector(".laptop");
+  const content = document.querySelector(".content");
+
+  laptop.style.right = 100 + e.pageX/2 + 'px';
+  content.style.left = 100 + e.pageX/2.5 + 'px';
+})
